9baf39ce4adfee7ff0339006695cb5bf41a3fcbd
Asier Salsidua <asier.salsidua@opendeusto.es>
2016-04-20 13:16:08 +0200

    hito4-final

--- src/hanoi/Ejecutable.java
+++ src/hanoi/Ejecutable.java
@@ -5,6 +5,7 @@ import es.deusto.ingenieria.is.search.algorithms.blind.DepthFS;
 import es.deusto.ingenieria.is.search.algorithms.heuristic.BestFS;
 import hanoi.formulation.HanoiEnvironment;
 import hanoi.formulation.HanoiProblem;
+import hanoi.heuristics.HillCimbing;
 import hanoi.heuristics.Leftys2EvaluationFunction;
 import hanoi.heuristics.LeftysEvaluationFunction;
 import hanoi.heuristics.SearchEvaluationFunction;
@@ -24,12 +25,16 @@ public class Ejecutable {
 		HanoiEnvironment environment = (HanoiEnvironment) problem.gatherInitialPercepts();
 		problem.addInitialState(environment);
 		System.out.println(environment.toString());		
-		/*problem.solve(DepthFS.getInstance());
+		problem.solve(DepthFS.getInstance());
 		problem.solve(BreadthFS.getInstance());
-		problem.solve(new BestFS(new SearchEvaluationFunction()));
+		//problem.solve(new BestFS(new SearchEvaluationFunction()));
 		System.out.println("\nTAKING INTO ACOUNT ONLY THE LEFT STICK");
 		problem.solve(new BestFS(new LeftysEvaluationFunction()));
 		System.out.println("\nTAKING INTO ACOUNT THE LEFT AND CENTRE STICKS");
-		problem.solve(new BestFS(new Leftys2EvaluationFunction()));*/
+		problem.solve(new BestFS(new Leftys2EvaluationFunction()));
+		System.out.println("\nHILL CLIMBING METHOD WITH LEFT STICK");
+		problem.solve(new HillCimbing(new LeftysEvaluationFunction()));
+		System.out.println("\nHILL CLIMBING METHOD WITH LEFT AND CENTRE STICKS");
+		problem.solve(new HillCimbing(new Leftys2EvaluationFunction()));
 	}
 }
--- src/hanoi/heuristics/HillCimbing.java
+++ src/hanoi/heuristics/HillCimbing.java
@@ -1,6 +1,7 @@
 package hanoi.heuristics;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import es.deusto.ingenieria.is.search.algorithms.Node;
@@ -17,9 +18,62 @@ public class HillCimbing extends HeuristicSearchMethod{
 	}
 
 	@Override
-	public Node search(Problem arg0, State arg1) {
-		Node node = new Node(arg1);
-		
-		return node;
+	public Node search(Problem problem, State initialState) {
+		List<Node> frontier = new ArrayList<Node>();
+		List<State> generatedStates = new ArrayList<State>();
+		List<State> expandedStates = new ArrayList<State>();
+		Node firstNode = null;
+		List<Node> successorNodes = null;
+		boolean solutionFound = false;	
+		frontier.add(new Node(initialState));
+		while (!solutionFound && !frontier.isEmpty()) {			
+			firstNode = frontier.remove(0);			
+			if (problem.isFinalState(firstNode.getState())) {
+				solutionFound = true;			
+			} else {
+				successorNodes = this.expand(firstNode, problem, generatedStates, expandedStates);
+				if (successorNodes != null && !successorNodes.isEmpty()) {
+					frontier.addAll(successorNodes);
+					Collections.sort(frontier);
+				}
+			}
+		}
+		if (solutionFound) {
+			return firstNode;
+		} else {
+			return null;
+		}
+	}
+	
+	protected List<Node> expand(Node node, Problem problem, List<State> generatedStates, List<State> expandedStates){
+		List<Node> successorNodes = new ArrayList<Node>();
+		Node successorNode=null;
+		State currentState=null;
+		State successorState=null;
+		if(node!=null && problem!=null){
+			currentState=node.getState();
+			generatedStates.remove(currentState);
+			expandedStates.add(currentState);
+			if(currentState!=null){
+				for(Operator operator : problem.getOperators()){
+					successorState = operator.apply(currentState);
+					if (successorState != null) {
+						if (!generatedStates.contains(successorState) && !expandedStates.contains(successorState)) {
+							if(this.getEvaluationFunction().calculateH(new Node(successorState))>this.getEvaluationFunction().calculateH(new Node(currentState))){
+								successorNode = new Node(successorState);
+								successorNode.setOperator(operator.getName());
+								successorNode.setParent(node);
+								successorNode.setDepth(node.getDepth() + 1);
+								successorNode.setH(this.getEvaluationFunction().calculateH(successorNode));
+								successorNodes.add(successorNode);
+								generatedStates.add(successorState);								
+							}else
+								break;
+						}
+					}
+				}
+			}
+		}
+		return successorNodes;
 	}
 }
